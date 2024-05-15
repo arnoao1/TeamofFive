f22e68de3777a20fa7e702ea476703551ad277d2
Aston Arnott (1000122523) <ARNOAO1@student.op.ac.nz>
2024-05-16 10:27:32 +1200

    Main blank code with Jay's Checkpoint

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TeamofFive.usb
+{
+    internal class teamproject
+    {
+        static void Main(string[] args)
+        {
+            Lab6Method()
+			
+		}
+		static void Lab6Method()
+		{
+			int Team1 = 0;
+            int Team2 = 0;
+
+            Random rand = new Random();
+            Team1 = rand.Next(10);
+            Team2 = rand.Next(10);
+            Console.WriteLine("Lets see how your favorite team played today");
+            Console.WriteLine();
+            Thread.Sleep(2000);
+            Console.WriteLine($"(Team1 scored {Team1}) - (Team2 (your favorite team) scored {Team2})");
+            Console.WriteLine();
+            if (Team1 != Team2)
+            {
+                if (Team1 > Team2)
+                {
+                    Console.WriteLine("Your Team Lost!");
+                }
+                else 
+                {
+                    Console.WriteLine("Your Team Wins!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Draw!");
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
